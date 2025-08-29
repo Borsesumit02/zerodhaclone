bd84675f81f206ff9dd2ecea04633d84fb65d89b
Borsesumit02 <98315735+Borsesumit02@users.noreply.github.com>
2025-08-29 18:18:41 +0530

    not visibal on phone

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import Products from './landingpage/products/Products.jsx'
 import PriceingPage from './landingpage/priceing/PriceingPage.jsx'
 import SupportPage from './landingpage/support/SupportPage.jsx'
 import Notfound from './landingpage/Notfound.jsx'
-import Notforview from './landingpage/notforview.jsx'
+import Notforview from './landingpage/Notforview.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
